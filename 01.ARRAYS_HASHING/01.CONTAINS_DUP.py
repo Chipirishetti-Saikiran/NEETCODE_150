6b7d103802577ed0a3e6ba6738c7a0fa59d40983
Chipirishetti-Saikiran <ch381911@gmail.com>
2025-05-29 12:59:08 +0530

    Contain Dup

---
+++ 01.ARRAYS_HASHING/01.CONTAINS_DUP.py
@@ -0,0 +1,30 @@
+#BRUTE 
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+
+#SORTING
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
+#HASHING 
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+#HASHSET 
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) < len(nums)        
